28e7adb2ec2cc18c49b4d52814f7853e19c4bec8
villaflaminio <vifla01@gmail.com>
2022-09-30 19:10:33 +0200

    Pipe message type

---
+++ NamedPipesFullDuplex/Utilities/PipeMessage.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedPipesFullDuplex.Utilities
+{
+
+    [Serializable]
+    public class PipeMessage
+    {
+        public string topic { get; set; }
+        public string Message { get; set; }
+
+        //constructor
+        public PipeMessage()
+        {
+
+        }
+        public PipeMessage(string topic, string message)
+        {
+            this.topic = topic;
+            this.Message = message;
+        }
+        public byte[] Serialize()
+        {
+            using (MemoryStream m = new MemoryStream())
+            {
+                using (BinaryWriter writer = new BinaryWriter(m))
+                {
+                    writer.Write(topic);
+                    writer.Write(Message);
+                }
+                return m.ToArray();
+            }
+        }
+
+        public static PipeMessage Deserialize(byte[] data)
+        {
+            PipeMessage result = new PipeMessage();
+
+            using (MemoryStream m = new MemoryStream(data))
+            {
+                using (BinaryReader reader = new BinaryReader(m))
+                {
+                    result.topic = reader.ReadString();
+                    result.Message = reader.ReadString();
+                }
+            }
+            return result;
+        }
+
+        //tostring
+        public override string ToString()
+        {
+            return string.Format("[topic: {0} , Message: {1}]", topic, Message);
+        }
+    }
+
+
+}
